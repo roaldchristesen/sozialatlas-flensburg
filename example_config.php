3a0076a7107bbcea2476e7ab93e7ef32f42e7205
Roald Christesen <roald@grain.one>
2023-09-25 14:37:34 +0200

    example_config.php added

---
+++ example_config.php
@@ -0,0 +1,9 @@
+<?php
+    // 1) Change the following values to connect with you PSQL database...
+    // 2) Rename file to config.php
+    $DB_HOST = 'your_host';
+    $DB_PORT = 5432;
+    $DB_NAME = 'your_name';
+    $DB_USER = 'your_db_user';
+    $DB_PWD = 'your_db_password';
+?>
